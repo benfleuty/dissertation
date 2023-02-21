c8d67b696d57403a1ed251f844b1e2ce724b2d41
Ben Fleuty <bfleuty@outlook.com>
2023-02-21 11:05:58 +0000

    Write filenames to SQL

---
+++ code/TranscodeNowWebServer/UploadedFilesLibrary/SqlDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace UploadFilesLibrary;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+	private readonly IConfiguration _config;
+
+	public SqlDataAccess(IConfiguration config)
+	{
+		_config = config;
+	}
+
+	public async Task SaveData(string storedProc,
+							string connName,
+							object parameters)
+	{
+		string connStr = _config.GetConnectionString(connName)
+			?? throw new Exception($"Missing connection string at {connName}");
+
+		using var conn = new SqlConnection(connStr);
+
+		await conn.ExecuteAsync(storedProc,
+						  parameters,
+						  commandType: System.Data.CommandType.StoredProcedure);
+
+
+	}
+}
