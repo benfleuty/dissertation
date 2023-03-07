abd5c9e5083221a6ee66b6cfe9c42c93f5653101
Ben Fleuty <bfleuty@outlook.com>
2023-03-07 11:24:17 +0000

    Brave causes crash

---
+++ code/TranscodeNowWebServer/UploadedFilesLibrary/FileUploader.cs
@@ -0,0 +1,33 @@
+﻿using FluentFTP;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace UploadedFilesLibrary;
+
+public class FileUploader
+{
+	private static FtpClient CreateFtpClient()
+	{
+		var builder = new ConfigurationBuilder()	
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddUserSecrets<FileUploader>();
+
+		var config = builder.Build();
+
+		string url = "localhost";
+		string username = config["FtpUserName"];
+		string password = config["FtpPassword"];
+		var creds = new NetworkCredential(username, password);
+		return new FtpClient(url, creds);
+	}
+
+
+	public static async Task<bool> UploadFile(string path)
+	{
+		FtpClient ftp = CreateFtpClient();
+		using FileStream fs = File.OpenRead(path);
+		var status = ftp.UploadFile(path, Path.GetFileName(path));
+		return status == FtpStatus.Success;
+	}
+}
---
+++ code/TranscodeNowWebServer/UploadedFilesLibrary/ISqlDataAccess.cs
@@ -0,0 +1,6 @@
+﻿namespace UploadedFilesLibrary;
+
+public interface ISqlDataAccess
+{
+	Task SaveData(string storedProc, string connName, object parameters);
+}
