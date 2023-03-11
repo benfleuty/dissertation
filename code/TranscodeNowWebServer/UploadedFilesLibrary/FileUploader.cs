bc23470a09a6d41c7c9b4babe097e5d77b70ec49
Ben Fleuty <bfleuty@outlook.com>
2023-03-11 15:41:19 +0000

    Fix upload to web server sequence

--- code/TranscodeNowWebServer/UploadedFilesLibrary/FileUploader.cs
+++ code/TranscodeNowWebServer/UploadedFilesLibrary/FileUploader.cs
@@ -1,7 +1,6 @@
 ﻿using FluentFTP;
 using Microsoft.Extensions.Configuration;
 using System.Net;
-using System.Net.NetworkInformation;
 
 namespace UploadedFilesLibrary;
 
@@ -9,24 +8,24 @@ public class FileUploader
 {
 	private static FtpClient CreateFtpClient()
 	{
-		var builder = new ConfigurationBuilder()	
+		var builder = new ConfigurationBuilder()
 			.SetBasePath(Directory.GetCurrentDirectory())
 			.AddUserSecrets<FileUploader>();
 
 		var config = builder.Build();
 
 		string url = "localhost";
-		string username = config["FtpUserName"];
-		string password = config["FtpPassword"];
+		string username = config["FtpUserName"] ?? throw new NullReferenceException();
+		string password = config["FtpPassword"] ?? throw new NullReferenceException();
 		var creds = new NetworkCredential(username, password);
 		return new FtpClient(url, creds);
 	}
 
 
-	public static async Task<bool> UploadFile(string path)
+	public static bool UploadFile(string path)
 	{
+
 		FtpClient ftp = CreateFtpClient();
-		using FileStream fs = File.OpenRead(path);
 		var status = ftp.UploadFile(path, Path.GetFileName(path));
 		return status == FtpStatus.Success;
 	}
