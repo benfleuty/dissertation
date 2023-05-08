c9cbdcc588135748b680b793b6a81f670d5b593c
Ben Fleuty <bfleuty@outlook.com>
2023-05-08 03:56:09 +0100

    WIP Implement audio options

---
+++ code/TranscodeNowWebServer/TranscodeNowWebServer/Pages/Options/Options.AudioOptions.cs
@@ -0,0 +1,18 @@
+﻿
+public partial class Options
+{
+    private int? _audioBitrate;
+    private string? _audioCodec;
+
+    public int? AudioBitrate
+    {
+        get => _audioBitrate;
+        set { _audioBitrate = value; }
+    }
+
+    public string? AudioCodec
+    {
+        get => _audioCodec;
+        set { _audioCodec = value; }
+    }
+}
