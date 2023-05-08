516686ec66164622e5d20fd048ca3ddd4870e0f8
Ben Fleuty <bfleuty@outlook.com>
2023-05-08 03:53:40 +0100

    WIP Implement video options

--- code/TranscodeNowWebServer/TranscodeNowWebServer/Pages/Options/Options.VideoOptions.razor.cs
+++ code/TranscodeNowWebServer/TranscodeNowWebServer/Pages/Options/Options.VideoOptions.razor.cs
@@ -3,26 +3,199 @@
 // Video
 public partial class Options
 {
+    private int? _cropBottom;
+    private int? _cropLeft;
+    private int? _cropRight;
+    private int? _cropTop;
+    private int? _endTimeHours;
+    private int? _endTimeMinutes;
+    private int? _endTimeSeconds;
     private double? _frameRate;
-    private int? _imageHeight = null;
-    private int? _imageWidth = null;
+    private bool? _hFlip;
+    private int? _imageHeight;
+    private int? _imageWidth;
 
     private string? _outputFormat;
 
-    private TimeSpan? _startTime;
+    private int? _rotation;
 
-    private TimeSpan? _stopTime;
+    private int? _startTimeHours;
+    private int? _startTimeMinutes;
+    private int? _startTimeSeconds;
 
-    private int? _videoBitrate;
+    private bool? _vFlip;
+    private long? _videoBitrate;
 
     private string? _videoCodec;
 
-    private TimeSpan? _videoDuration;
+    public int? CropBottom
+    {
+        get { return _cropBottom; }
+        set
+        {
+            if (value is null)
+            {
+                _cropBottom = null;
+                return;
+            }
+            int val = value.Value;
+            int top = CropTop ?? 0;
+            int max = InitialVideoStream.Height;
+
+            if (val + top > max) val = (int)CropBottom;
+            else if (val < 0) val = 0;
+
+            _cropBottom = val;
+            StateHasChanged();
+        }
+    }
+
+    public int? CropLeft
+    {
+        get { return _cropLeft; }
+        set
+        {
+            if (value is null)
+            {
+                _cropLeft = null;
+                return;
+            }
+            int val = value.Value;
+            int right = CropRight ?? 0;
+            int max = InitialVideoStream.Width;
+
+            if (val + right > max) val = (int)CropLeft;
+            else if (val < 0) val = 0;
+
+            _cropLeft = val;
+            StateHasChanged();
+        }
+    }
+
+    public int? CropRight
+    {
+        get { return _cropRight; }
+        set
+        {
+            if (value is null)
+            {
+                _cropRight = null;
+                return;
+            }
+            int val = value.Value;
+            int left = CropLeft ?? 0;
+            int max = InitialVideoStream.Width;
+
+            if (val + left > max) val = (int)CropRight;
+            else if (val < 0) val = 0;
+
+            _cropRight = val;
+            StateHasChanged();
+        }
+    }
+
+    public int? CropTop
+    {
+        get { return _cropTop; }
+        set
+        {
+            if (value is null)
+            {
+                _cropTop = null;
+                return;
+            }
+            int val = value.Value;
+            int bottom = CropBottom ?? 0;
+            int max = InitialVideoStream.Height;
+
+            if (val + bottom > max) val = (int)CropTop;
+            else if (val < 0) val = 0;
+
+            _cropTop = val;
+            StateHasChanged();
+        }
+    }
+
+    public int? EndTimeHours
+    {
+        get => _endTimeHours;
+        set
+        {
+            if (value is null)
+            {
+                _endTimeHours = null;
+                return;
+            }
+            int result = value.Value;
+            if (value < 0) result = 0;
+            else if (value > 10) result = 10;
+
+            if (result < StartTimeHours)
+                result = StartTimeHours.GetValueOrDefault(0);
+
+            _endTimeHours = result;
+        }
+    }
+
+    public int? EndTimeMinutes
+    {
+        get => _endTimeMinutes;
+        set
+        {
+            if (value is null)
+            {
+                _endTimeMinutes = null;
+                return;
+            }
+            int result = value.Value;
+            if (value < 0) result = 0;
+            else if (value > 10) result = 10;
+
+            if (result < StartTimeMinutes &&
+                EndTimeHours <= StartTimeHours)
+                result = StartTimeMinutes;
+
+            _endTimeMinutes = result;
+        }
+    }
+
+    public int? EndTimeSeconds
+    {
+        get => _endTimeSeconds;
+        set
+        {
+            int result = value;
+            if (string.IsNullOrEmpty(result.ToString())) result = 0;
+            else if (value < 0) result = 0;
+            else if (value > 10) result = 10;
+
+            if (result < StartTimeSeconds &&
+                (EndTimeHours <= StartTimeHours || EndTimeMinutes <= StartTimeMinutes))
+                result = StartTimeSeconds;
+
+            _endTimeSeconds = result;
+        }
+    }
 
     public double? FrameRate
     {
-        get { return _frameRate; }
-        set { _frameRate = value; }
+        get => _frameRate;
+        set
+        {
+            double result = value ?? _frameRate ?? InitialVideoStream.FrameRate;
+            if (result < 1) result = 1;
+            else if (result > 120) result = 120;
+            _frameRate = result;
+        }
+    }
+
+    public bool? HFlip
+    {
+        get => _hFlip;
+        set
+        {
+            _hFlip = value;
+        }
     }
 
     public int? ImageHeight
@@ -30,10 +203,12 @@ public partial class Options
         get => _imageHeight;
         set
         {
-            if (!int.TryParse(value.ToString(), out int result)) return;
-            _imageHeight = result;
-            AlteredVideoStream!.Height = result;
-            StateHasChanged();
+            int val = value ?? _imageHeight ?? InitialVideoStream.Height;
+            if (val > 4096) val = 4096;
+            if (val < 1) val = 1;
+
+            _imageHeight = val;
+            AlteredVideoStream!.Height = val;
         }
     }
 
@@ -42,89 +217,123 @@ public partial class Options
         get => _imageWidth;
         set
         {
-            if (!int.TryParse(value.ToString(), out int result)) return;
-            _imageWidth = result;
-            AlteredVideoStream!.Width = result;
-            StateHasChanged();
+            int val = value ?? _imageWidth ?? InitialVideoStream.Width;
+
+            if (val > 4096) val = 4096;
+            if (val < 1) val = 1;
+
+            _imageWidth = val;
+            AlteredVideoStream!.Width = val;
         }
     }
     public string? OutputFormat
     {
-        get { return _outputFormat; }
+        get => _outputFormat;
         set { _outputFormat = value; }
     }
-
-    public TimeSpan? StartTime
+    public int? Rotation
     {
-        get { return _startTime; }
-        set { _startTime = value; }
+        get => _rotation;
+        set
+        {
+            int val = value ?? _rotation ?? InitialVideoStream.Rotation;
+
+            if (val < 0) val += 360;
+            if (val > 359) val = 359;
+
+            _rotation = val;
+        }
     }
 
-    public TimeSpan? StopTime
+    public int? StartTimeHours
     {
-        get { return _stopTime; }
-        set { _stopTime = value; }
+        get => _startTimeHours;
+        set
+        {
+            if (value is null)
+            {
+                _startTimeHours = null;
+                return;
+            }
+            int result = value.Value;
+            if (value < 0) result = 0;
+            else if (value > 10) result = 10;
+
+            _startTimeHours = result;
+        }
+    }
+    public int? StartTimeMinutes
+    {
+        get => _startTimeMinutes;
+        set
+        {
+            if (value is null)
+            {
+                _startTimeMinutes = null;
+                return;
+            }
+            int result = value.Value;
+            TimeSpan fileTime = InitialVideoStream.Duration;
+            int hours = fileTime.Hours;
+            if (value < 0) result = 0;
+            else if (value > 59) result = 59;
+
+            if (hours == 0)
+            {
+                if (result > fileTime.Minutes) result = fileTime.Minutes;
+            }
+            _startTimeMinutes = result;
+        }
+    }
+    public int? StartTimeSeconds
+    {
+        get => _startTimeSeconds;
+        set
+        {
+            if (value is null)
+            {
+                _startTimeSeconds = null;
+                return;
+            }
+            int result = value.Value;
+            TimeSpan fileTime = InitialVideoStream.Duration;
+            int mins = fileTime.Minutes;
+            if (string.IsNullOrEmpty(result.ToString())) result = 0;
+            else if (value < 0) result = 0;
+            else if (value > 59) result = 59;
+
+            if (mins == 0)
+            {
+                if (result > fileTime.Seconds) result = fileTime.Seconds;
+            }
+            _startTimeSeconds = result;
+        }
+    }
+    public bool? VFlip
+    {
+        get => _vFlip;
+        set
+        {
+            _vFlip = value;
+        }
     }
 
-    public int? VideoBitrate
+    public long? VideoBitrate
     {
-        get { return _videoBitrate; }
-        set { _videoBitrate = value; }
+        get => _videoBitrate;
+        set
+        {
+            long val = value ?? _videoBitrate ?? InitialVideoStream.BitRate;
+
+            if (val > 50000) val = 50000;
+            else if (val < 0) val = 0;
+
+            _videoBitrate = val;
+        }
     }
     public string? VideoCodec
     {
-        get { return _videoCodec; }
+        get => _videoCodec;
         set { _videoCodec = value; }
-    }
-
-    public TimeSpan? VideoDuration
-    {
-        get { return _videoDuration; }
-        set { _videoDuration = value; }
-    }
-
-    private int? _rotation;
-
-    public int? Rotation
-    {
-        get { return _rotation; }
-        set { _rotation = value; }
-    }
-
-    private bool? _hFlip;
-
-    public bool? HFlip
-    {
-        get { return _hFlip; }
-        set { _hFlip = value; }
-    }
-
-    private bool? _vFlip;
-
-    public bool? VFlip
-    {
-        get { return _vFlip; }
-        set { _vFlip = value; }
-    }
-
-
-}
-
-// Audio
-public partial class Options
-{
-    private int? _audioBitrate;
-    private string? _audioCodec;
-
-    public int? AudioBitrate
-    {
-        get { return _audioBitrate; }
-        set { _audioBitrate = value; }
-    }
-
-    public string? AudioCodec
-    {
-        get { return _audioCodec; }
-        set { _audioCodec = value; }
     }
 }
